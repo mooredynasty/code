635d14d3e5ad91326fd47e664390f75237144f02
Marc Moore <marc@mooredynasty.com>
2015-10-08 11:24:13 -0500

    Pushing new changes

--- source/web/WebHandlers.cs
+++ source/web/WebHandlers.cs
@@ -1,10 +1,20 @@
-﻿namespace code.web
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace code.web
 {
   public class WebHandlers : IGetWebRequestHandlers
   {
+    IEnumerable<IHandleOneWebRequest> handlers;
+
+    public WebHandlers(IEnumerable<IHandleOneWebRequest> handlers)
+    {
+        this.handlers = handlers;
+    }
+
     public IHandleOneWebRequest get_handler_for_request(IProvideDetailsToHandlers request)
     {
-      throw new System.NotImplementedException();
+        return this.handlers.Where(h => h.can_process(request)).First();
     }
   }
 }
